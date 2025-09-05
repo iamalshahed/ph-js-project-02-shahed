e434cc711a3a31f7332cea4a4ee5505f567b4558
iamalshahed <shahedevise@gmail.com>
2025-09-05 20:21:53 +0600

    33-3 Load and Display All Lessons with API as Figma

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
-console.log("connected");
+const loadLesson = () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+
+  fetch(url) // return kore response er jonno.. promise of response. bole je vai thik ache ami tomake response dibo.. but direct kono data/value return kore na
+    .then((res) => res.json()) // eita amake promise je ok vai ami tomake server theke pawa data-gulo json akare covert kore dibo!! 'kono value return kore na.. amake promise kore json data dewar jonno' --->> promise of json() data
+    .then((json) => displayLesson(json.data)); // jei json data gulo pabo seigu ekta function er modde store kore diyechi.. eikhane 'json' name er parameter e sokol data store hobe
+};
+
+// datagulo ke display korte hobe
+const displayLesson = (lessons) => {
+  // 1. get the parent & empty innerHTML
+  const levelContainer = document.getElementById("level__container");
+  levelContainer.innerHTML = "";
+
+  // 2. get into every lesson --->> loop chalano lagbe
+  for (let lesson of lessons) {
+    // 3. create an element
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary">
+            <span>
+            <i class="ri-book-open-fill"></i>
+            </span>
+            Lesson - ${lesson.level_no}
+        </button>
+    `;
+
+    // 4. appendChild --->> jei element ta create korlam seita ekhn parent div er sathe append kore ditesi
+    levelContainer.appendChild(btnDiv);
+  }
+};
+
+loadLesson();
